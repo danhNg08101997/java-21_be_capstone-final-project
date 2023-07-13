119d177a4bc5b07183ba3f4f24a1837715685d45
danhNg.8 <105102066+danhNg08101997@users.noreply.github.com>
2023-07-13 14:21:09 +0700

    create project

---
+++ src/main/java/com/cybersoft/newbalanceproject/NewbalanceprojectApplication.java
@@ -0,0 +1,13 @@
+package com.cybersoft.newbalanceproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewbalanceprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NewbalanceprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cybersoft/newbalanceproject/NewbalanceprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cybersoft.newbalanceproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewbalanceprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
